85cfd3f6ecf30433965d1ea4553e30a6c65cc75d
yyrlsh520 <yuyr_cumt@126.com>
2016-10-26 23:06:14 +0800

    添加单元测试函数

--- src/gyGame.java
+++ src/gyGame.java
@@ -1,5 +1,7 @@
 import static org.junit.Assert.*;
 
+import java.util.HashMap;
+
 import org.junit.Test;
 
 
@@ -7,16 +9,48 @@ public class gyGame {
 
 	@Test
 	public void testEmptyStr() {
+		// ²âÊÔ¿Õ×Ö·û´®
+		String inputString = "";
 		
-		fail("Not yet implemented");
+		String ret = maxSubStr(inputString);
+		assertEquals(ret.length(), 0);
 	}
 	
-	public void testMaxLenthStr() {
+	public void testStrLenthEqualTo30000() {
+		// ²âÊÔ×î´ó³¤¶È×Ö·û´®
 		
-	}
-	
-	public void test() {
+		String inputString = "";
+		for(int i=0;i<1000;i++){
+			inputString+="qwertyuioplkjhgfdsazxcvbnmqwer";
+		}
 		
+		String ret = maxSubStr(inputString);
+		assertEquals(ret.length(), 26);
 	}
 
+	public void testStrLenthEqualTo1500() {
+		String inputString = "";
+		for(int i=0;i<100;i++){
+			inputString+="qwertyuioplkjhg";
+		}
+		
+		String ret = maxSubStr(inputString);
+		assertEquals(ret.length(), 15);
+	}
+
+	public void testStrLenthEqualTo100() {
+		String inputString = "";
+		for(int i=0;i<10;i++){
+			inputString+="qwertyuiop";
+		}
+		
+		String ret = maxSubStr(inputString);
+		assertEquals(ret.length(), 10);
+	}
+	public void testStrLenthEqualTo10() {
+		String inputString = "qweradgqqw";
+		
+		String ret = maxSubStr(inputString);
+		assertEquals(ret.length(), 7);
+	}
 }
