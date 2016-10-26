be1004be487ccea2d41df01bee8bc810582dbe8f
yyrlsh520 <yuyr_cumt@126.com>
2016-10-26 22:46:43 +0800

    init repository

---
+++ src/gyGame.java
@@ -0,0 +1,22 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+
+public class gyGame {
+
+	@Test
+	public void testEmptyStr() {
+		
+		fail("Not yet implemented");
+	}
+	
+	public void testMaxLenthStr() {
+		
+	}
+	
+	public void test() {
+		
+	}
+
+}
